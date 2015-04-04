f97164541eebd51675ed86f60c2d348c8bdcd8d9
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-04-04 18:20:18 +0700

    Separated app into a directory

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session);
 app.use(baseurl);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // route mapping
 app.use('/', routes);
