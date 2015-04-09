fa4941f4182b13de658856ff8bc766ca31d288d2
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-04-09 18:26:41 +0700

    Added random token field for Appointment

--- app/controllers/PublicCalendar.js
+++ app/controllers/PublicCalendar.js
@@ -17,6 +17,9 @@ var PublicCalendarController = {
   },
   postBooking: function (req, res, next) {
     var appointment = req.body.appointment;
+    appointment.appointment.token = appointment.slot.id + '' +
+        require('crypto').randomBytes(30).toString('hex');
+
     models.Slot.find({ where: appointment.slot })
       .then(function(slot) {
         slot.update({
--- app/models/Appointment.js
+++ app/models/Appointment.js
@@ -2,7 +2,8 @@ module.exports = function (sequelize, DataTypes) {
   var Appointment = sequelize.define('Appointment', {
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false },
-    phone: { type: DataTypes.STRING, allowNull: false }
+    phone: { type: DataTypes.STRING, allowNull: false },
+    token: { type: DataTypes.STRING, allowNull: false }
   }, {
     classMethods: {
       associate: function(models) {
