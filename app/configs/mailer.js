b4ec8c8f2a1a3031f63160c772d9a687f5419232
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-04-11 18:18:27 +0700

    Ask for reschedule mailer done

--- app/configs/mailer.js
+++ app/configs/mailer.js
@@ -1,3 +1,6 @@
+var jade = require('jade');
+var path = require('path');
+
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   host: 'localhost',
@@ -5,8 +8,35 @@ var transporter = nodemailer.createTransport({
   ignoreTLS: true
 });
 
+var mailer = {
+  _transporter: transporter,
+  _sender: 'no-reply@ppl-b02.cs.ui.ac.id',
+  _templatePath: path.join(__dirname, '../views/emails'),
+
+  sendAskForReschedule: function (to, reason, token) {
+    this._transporter.sendMail({
+      from: this._sender,
+      to: to,
+      subject: '[Appointer] You have been asked for a reschedule',
+      html: '<h1>You have been asked for a reschedule</h1>\
+             <p>The reason is: </p>\
+             <p><blockquote>' + reason + '</blockquote></p>\
+             <p>For reschedule, visit\
+              <a href="http://ppl-b02.cs.ui.ac.id/appointer/reschedule/' + token + '">\
+                http://ppl-b02.cs.ui.ac.id/appointer/reschedule/' + token + '\
+              </a>\
+             </p>'
+    }, function(err, info) {
+      if (err) console.log(err);
+      else console.log('Message sent:', info.response);
+    });
+  },
+
+
+};
+
 module.exports = function (req, res, next) {
-  res.locals.mailer = transporter;
+  res.locals.mailer = mailer;
   res.locals.sender = 'no-reply@ppl-b02.cs.ui.ac.id';
   next();
 };
